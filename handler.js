da639f753e303e286112648976341f156a298339
Brian Grant <brian@skeletonjack.net>
2018-12-03 22:15:50 -0600

    End of "Logs and Monitoring..."

--- handler.js
+++ handler.js
@@ -1,14 +1,12 @@
 'use strict';
 
 module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+  console.log(event.Records[0].s3);
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+
+  console.log(`A file named ${key} was put in a bucket ${bucket}`);
+
+  return { message: `A file named ${key} was put in a bucket ${bucket}`, event };
 };
